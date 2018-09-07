62a958c332ccbfa6785b17502dc7aae27ffecea0
jossuehernandez <jossuehernandez@users.noreply.github.com>
2018-09-07 06:01:01 -0500

    1. Localización actual del usuario.

--- GoogleMapsDemo/app/src/main/java/dalvik/com/mx/googlemapsdemo/MapActivity.java
+++ GoogleMapsDemo/app/src/main/java/dalvik/com/mx/googlemapsdemo/MapActivity.java
@@ -3,16 +3,30 @@ package dalvik.com.mx.googlemapsdemo;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+
+
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+
+import android.util.Log;
 
 public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
 
@@ -21,8 +35,12 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
     private static final String COURSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
     private  static final int LOCATION_PERMISSION_REQUEST_CODE = 1234;
 
-    private Boolean mLocationPermissionGranted = false;
+    private static final float DEFAULT_ZOOM = 15f;
+
+
+    private Boolean mLocationPermissionsGranted = false;
     private GoogleMap mMap;
+    private FusedLocationProviderClient mFusedLocationProviderClient;
 
     @Override
     protected void onCreate( @Nullable Bundle savedInstanceState ){
@@ -33,16 +51,63 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
     }
 
 
+    private void getDeviceLocation(){
+        Log.d(TAG, "getDeviceLocation: getting the devices current location");
+
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
+        try{
+            if(mLocationPermissionsGranted){
+
+                final Task location = mFusedLocationProviderClient.getLastLocation();
+                location.addOnCompleteListener(new OnCompleteListener() {
+                    @Override
+                    public void onComplete(@NonNull Task task) {
+                        if(task.isSuccessful()){
+                            Log.d(TAG, "onComplete: found location!");
+                            Location currentLocation = (Location) task.getResult();
+
+                            moveCamera(new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude()),
+                                    DEFAULT_ZOOM);
+
+                        }else{
+                            Log.d(TAG, "onComplete: current location is null");
+                            Toast.makeText(MapActivity.this, "unable to get current location", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        }catch (SecurityException e){
+            Log.e(TAG, "getDeviceLocation: SecurityException: " + e.getMessage() );
+        }
+    }
+
+    private void moveCamera(LatLng latLng, float zoom){
+        Log.d(TAG, "moveCamera: moving the camera to: lat: " + latLng.latitude + ", lng: " + latLng.longitude );
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoom));
+    }
+
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
+
+        Toast.makeText(this, "Map is Ready", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "onMapReady: map is ready");
         mMap = googleMap;
+
+        if (mLocationPermissionsGranted) {
+            getDeviceLocation();
+        }
     }
 
     private void getLocationPermission() {
         String [] permissions = {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION };
         if( ContextCompat.checkSelfPermission( this.getApplicationContext(), FINE_LOCATION ) == PackageManager.PERMISSION_GRANTED ){
             if( ContextCompat.checkSelfPermission( this.getApplicationContext(), COURSE_LOCATION ) == PackageManager.PERMISSION_GRANTED ){
-                mLocationPermissionGranted = true;
+                mLocationPermissionsGranted = true;
+
+                initMap();
+
             }
             else
                 ActivityCompat.requestPermissions(this, permissions, LOCATION_PERMISSION_REQUEST_CODE );
@@ -54,18 +119,18 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
 
 
     public void onRequestPermissionResult( int requestCode,@NonNull String[] permissions, @NonNull int[] grantResults ){
-        mLocationPermissionGranted = false;
+        mLocationPermissionsGranted = false;
 
         switch( requestCode ){
             case LOCATION_PERMISSION_REQUEST_CODE: {
                 if( grantResults.length > 0 ){
                     for( int i = 0; i < grantResults.length; i++ ){
                         if( grantResults[i] != PackageManager.PERMISSION_GRANTED ){
-                            mLocationPermissionGranted = false;
+                            mLocationPermissionsGranted = false;
                             return;
                         }
                     }
-                    mLocationPermissionGranted = true;
+                    mLocationPermissionsGranted = true;
 
                     initMap();
                 }
@@ -74,7 +139,6 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
     }
 
     private void initMap() {
-
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById( R.id.map );
         mapFragment.getMapAsync( MapActivity.this );
     }
