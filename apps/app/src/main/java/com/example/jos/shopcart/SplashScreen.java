d13f055e3fba300e9d23146eb3813cabae135757
jossuehernandez <jossuehernandez@users.noreply.github.com>
2018-09-14 05:13:14 -0500

    ADR-JVA-050	Splash screen. Full Screen.
    ADR-JVA-050	Splash screen. Maquetación UI.
    
    También se debe quitar el ActionBar.

--- apps/app/src/main/java/com/example/jos/shopcart/SplashScreen.java
+++ apps/app/src/main/java/com/example/jos/shopcart/SplashScreen.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -14,6 +16,11 @@ public class SplashScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_splash);
 
         new Handler().postDelayed(new Runnable() {
