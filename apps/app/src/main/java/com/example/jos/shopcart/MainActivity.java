87e2b8fafb6ff0ffebee20b94ea9e44345685a5c
jossuehernandez <jossuehernandez@users.noreply.github.com>
2018-09-14 04:47:31 -0500

    ADR-JVA-051. SignIn. Creación y cambio a Activity SignIn desde MainActivity.
    ADR-JVA-051. Login. SignIn. Maquetación UI.

--- apps/app/src/main/java/com/example/jos/shopcart/MainActivity.java
+++ apps/app/src/main/java/com/example/jos/shopcart/MainActivity.java
@@ -1,13 +1,32 @@
 package com.example.jos.shopcart;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
+    }
+
+    private void init() {
+        Button btnMap = findViewById( R.id.btnSignIn );
+
+        btnMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent( MainActivity.this, SignIn.class );
+                startActivity( intent );
+            }
+        });
     }
 }
