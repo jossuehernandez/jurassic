898c8a5fd8055fee348930f9cf9158196968396b
jossuehernandez <jossuehernandez@users.noreply.github.com>
2018-09-18 09:35:10 -0500

    TextView

--- apps/app/src/main/java/com/example/jos/shopcart/HomeActivity.java
+++ apps/app/src/main/java/com/example/jos/shopcart/HomeActivity.java
@@ -3,10 +3,16 @@ package com.example.jos.shopcart;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class HomeActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView( R.layout.activity_home);
+
     }
+
+
 }
--- apps/app/src/main/java/com/example/jos/shopcart/SignIn.java
+++ apps/app/src/main/java/com/example/jos/shopcart/SignIn.java
@@ -1,15 +1,133 @@
 package com.example.jos.shopcart;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class SignIn extends AppCompatActivity {
 
     private static final String TAG = "SignIn";
+    private TextView mTextView;
+
+    private TextView mEmail;
+    private TextView mPasswd;
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    DatabaseReference mDatabaseReference = FirebaseDatabase.getInstance().getReference();
+    DatabaseReference mRootChild = mDatabaseReference.child("texto");
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView( R.layout.activity_sign_in);
+        mTextView = findViewById( R.id.txtMsg );
+        mAuth = FirebaseAuth.getInstance();
+        mEmail = findViewById(R.id.fieldEmail);
+        mPasswd = findViewById(R.id.fieldPasswd);
+
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+
+                if(firebaseAuth.getCurrentUser()!=null){
+
+                    Intent intent = new Intent( SignIn.this, HomeActivity.class );
+                    startActivity( intent );
+
+                }
+            }
+        };
+
+        Button btnSignIn = findViewById( R.id.btnSignIn );
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                login();
+
+
+            }
+        });
     }
+
+    @Override
+    protected void onStart() {
+
+        super.onStart();
+
+        mAuth.addAuthStateListener(mAuthListener);
+
+
+
+
+
+
+        mRootChild.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String texto = dataSnapshot.getValue().toString();
+                mTextView.setText( texto );
+                Log.d(TAG, "____________" );
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+    }
+
+    private void login(){
+
+        String email = mEmail.getText().toString();
+        String password = mPasswd.getText().toString();
+
+        if( email.equals("") || password.equals("") ) return;
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(SignIn.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
 }
